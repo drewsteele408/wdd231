853aa2879e363432e6378438776ba1a275e39e11
drewsteele408 <drewsteele408@gmail.com>
2025-05-24 15:46:53 -0600

    nps finishes

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
+import "../css/style.css"; // we can do this because we are using Vite...
+import "../css/home.css";
 import { getParkData, getInfoLinks } from "./parkService.mjs";
 import setHeaderFooter from "./setHeaderFooter.mjs";
 import { mediaCardTemplate } from "./templates.mjs";
@@ -10,9 +12,9 @@ function setParkIntro(data) {
 
 function setParkInfoLinks(data) {
   const infoEl = document.querySelector(".info");
-
+  // we have multiple links to build...so we map to transform the array of objects into an array of HTML strings.
   const html = data.map(mediaCardTemplate);
-  
+  // join the array of strings into one string and insert it into the section
   infoEl.insertAdjacentHTML("afterbegin", html.join(""));
 }
 
